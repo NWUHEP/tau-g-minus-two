acbf60acb227075ab827735435baddea971731bc
michaelmackenzie <30634463+michaelmackenzie@users.noreply.github.com>
2017-08-02 10:00:44 -0500

    Update eeminusbeam.cpp

--- eeminusbeam.cpp
+++ eeminusbeam.cpp
@@ -3,15 +3,15 @@
 #include "gnuplot.h"
 #include "eeminustopairs.h"
 
+/* print the 4-vector components */
 void printLVector(LVector & lV) {
   for (int i = 1; i < 4; i++) {
     cout << " " << lV[i];
   }
   cout << " " << lV[0];
-//  cout << lV;
 }
 
-        
+/*For printing out information in LHE format, can redirect code's output to lhef */ 
 void generateLHE(pandorasBox & box, double energy){
 
 
@@ -44,6 +44,7 @@ void generateLHE(pandorasBox & box, double energy){
        cout << "</LesHouchesEvents>";
 }
 
+/*For generating events to test with */
 void generateEvents(pandorasBox & box){
 
 
@@ -66,43 +67,28 @@ int main(int argv, char* argc[]){
 
        double ECM = atof(argc[1]);
 
-       //Energy of beam, electron beam polarization, laser polarization, initial particle(photon), final particle(photon/electron), and optionally the value of xzero (defaulted to xTelnov = 2 + sqrt(8)
-//       simplepbeam B1(ECM/2.0, 0);
-//       simplepbeam B2(ECM/2.0, 0);
        ebeam B1(ECM/2.0, -.8, electron, electron);
        ebeam B2(ECM/2.0, -.8, positron, positron);
 
-       /*To get peak at enpoint, set ePolarization = - lPolarization
-       for + helicity photons at high energy, take ePolarization > 0
 
-       xzero = 4 E(beam) * w(laser) / m_e^2
+         //Can turn off ISR         
+//       B1.ISRoff(); 
+//       B2.ISRoff();
 
-       Electron treatment has ISR but not beamstrahlung
-        */
-
-
-         //Can turn off ISR and Compton effects(for delta function of photon energy)          
-//       B1.ISRoff(); B1.Comptonoff();
-//       B2.ISRoff(); B2.Comptonoff();
-
-       //theta min
+       //theta min = 0.0
        eeminustollbar rxn5(0.0);
 
        pandora P(B1,B2,rxn5);
-       //pandora P2(B1, B2, rxn6);
-       //pandora P3(B1, B2, rxn7);
 
-       P.quiet();
+       P.quiet(); //turn of preparing print statements
        int Nprepare = 100000;
 
        P.prepare(Nprepare);
-       //P2.prepare(Nprepare);
-       //P3.prepare(Nprepare);
 
        pandorasBox box;
-       box.add(P); //box.add(P2); box.add(P3);
+       box.add(P);
 
-
+       //switching between printing lhe format and test event format
        generateEvents(box);
 //       generateLHE(box, ECM);
        return 0;
